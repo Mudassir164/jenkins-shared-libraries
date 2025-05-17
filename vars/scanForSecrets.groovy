6fcfe8029c512813b1fd19b943af1ec9bcaaf13a
Mudassir Ahmed <52211617+Mudassir164@users.noreply.github.com>
2025-05-17 15:33:05 +0500

    Create scanForSecrets.groovy

---
+++ vars/scanForSecrets.groovy
@@ -0,0 +1,27 @@
+// vars/scanForSecrets.groovy
+
+def call(String gitRepoUrl, String gitBranch, String maxDepth) {
+    // Run TruffleHog and save the output to a file
+    def result = sh(script: """
+        ~/bin/trufflehog git ${gitRepoUrl} --branch ${gitBranch} ${maxDepth} --json > secrets_scan_results.json  2>&1
+        jq '.' secrets_scan_results.json > output_secret_scan.json
+    """, returnStdout: true).trim()
+
+    // Read the contents of the secrets_scan_results.json to check for secrets
+    def verifiedSecret = sh(script: """
+        jq '.verified_secrets | select(. != null)' secrets_scan_results.json
+    """, returnStdout: true).trim()
+
+    def unverifiedSecret = sh(script: """
+        jq '.unverified_secrets | select(. != null)' secrets_scan_results.json
+    """, returnStdout: true).trim()
+
+    // Debug output
+    echo "Verified secrets: ${verifiedSecret}"
+    echo "Unverified secrets: ${unverifiedSecret}"
+
+    // Check if any secrets are found
+    if (verifiedSecret != "0" || unverifiedSecret != "0") {
+        error 'Secrets found in the repository! Failing the build.'
+    }
+}
