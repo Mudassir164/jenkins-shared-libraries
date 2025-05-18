726062621530a3ca57267c50d73adfbef5d72641
Mudassir Ahmed <52211617+Mudassir164@users.noreply.github.com>
2025-05-18 22:47:39 +0500

    Update scanForSecrets.groovy

--- vars/scanForSecrets.groovy
+++ vars/scanForSecrets.groovy
@@ -6,6 +6,9 @@ def call(String gitRepoUrl, String gitBranch, String maxDepth) {
         ~/bin/trufflehog git ${gitRepoUrl} --branch ${gitBranch} ${maxDepth} --json > secrets_scan_results.json  2>&1
     """, returnStdout: true).trim()
 
+    sh(script: """
+        jq '.' secrets_scan_output.json
+    """, returnStdout: true).trim()
     // Read the contents of the secrets_scan_results.json to check for secrets
     def verifiedSecret = sh(script: """
         jq '.verified_secrets | select(. != null)' secrets_scan_results.json
